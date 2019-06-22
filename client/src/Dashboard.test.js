996fdf01f7a32b372d542b93d1798b6123b688fc
Ronny Alvarado <rsalvarado777@gmail.com>
2019-06-22 12:26:15 -0500

    Only added a couple of tests...nothing crazy

---
+++ client/src/Dashboard.test.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import "@testing-library/react/cleanup-after-each";
+import "jest-dom/extend-expect";
+import Dashboard from "./Dashboard";
+
+it("renders without crashing", () => {
+  render(<Dashboard />);
+});
